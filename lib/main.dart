14d4952060b482c3b7d29ce97447e2142af7aef1
Sampaio-Luan <luansampaio.dev@gmail.com>
2024-03-26 22:36:53 -0300

    criado projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:lista_de_compras/meu_app.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) =>  const MyApp(),
+    ),
+  );
+}
 
 
 
