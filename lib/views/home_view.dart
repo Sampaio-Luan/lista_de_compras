f14cb1956ad237da8a4e23ec66b7c97ea45d3a72
Sampaio-Luan <luansampaio.dev@gmail.com>
2024-03-26 23:39:58 -0300

    Editando Home Page

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -10,44 +10,107 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
+  TextEditingController titulo = TextEditingController();
+  TextEditingController descricao = TextEditingController();
+  TextEditingController quantidade = TextEditingController();
+  TextEditingController listaCompras = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-       
-        title: Text(widget.title),
-      ),
-      body: Center(
-       
-        child: Column(
-      
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green[700],
+          automaticallyImplyLeading: false,
+          title: Text(
+            widget.title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 28,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
+          centerTitle: true,
         ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return novaLista();
+                });
+          }, //novaLista(),
+          tooltip: 'Increment',
+          label: const Text('Nova Lista',style: TextStyle(color: Colors.white, fontSize: 16),),
+          icon: const Icon(Icons.add, color: Colors.white,),
+          backgroundColor: Colors.green[700],
+          
+          
+        
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  AlertDialog novaLista() {
+    return AlertDialog(
+      elevation: 0,
+      actionsAlignment: MainAxisAlignment.spaceBetween,
+      title: const Text(
+        'Nova Lista de Compras',
+        textAlign: TextAlign.center,
+      ),
+      content: TextFormField(
+        controller: listaCompras,
+        style: const TextStyle(fontSize: 16),
+        decoration: const InputDecoration(
+            labelText: 'Nome da Lista',
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+              borderRadius: BorderRadius.all(
+                Radius.circular(25.0),
+              ),
+            ),
+            prefixIcon: Icon(Icons.shopping_cart_outlined)),
+        validator: (value) {
+          if (value == null) {
+            return 'Preenchimento obrigatorio';
+          } else if (value.isEmpty) {
+            return 'Preenchimento obrigatorio';
+          }
+          return null;
+        },
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            listaCompras.text = '';
+            Navigator.pop(context);
+          },
+          child: const Text("Cancelar"),
+        ),
+        TextButton(
+          onPressed: () {
+            debugPrint(listaCompras.text);
+
+            Navigator.pop(context);
+          },
+          child: const Text("Adicionar"),
+        ),
+      ],
     );
   }
 }
